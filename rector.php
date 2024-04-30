b467eee96654d680efa1479768df5b0d6a628f8f
Dominique Mastenbroek <dominique@goedemiddag.nl>
2024-04-30 10:43:35 +0200

    Update rector to ^1.0

--- rector.php
+++ rector.php
@@ -3,43 +3,33 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Isset_\IssetOnPropertyObjectToPropertyExistsRector;
-use Rector\CodeQuality\Rector\PropertyFetch\ExplicitMethodCallOverMagicGetSetRector;
 use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
 use Rector\Config\RectorConfig;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\ArrayShapeFromConstantArrayReturnRector;
 use RectorLaravel\Set\LaravelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::TYPE_DECLARATION,
+return RectorConfig::configure()
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true
+    )
+    ->withSets([
         LaravelSetList::LARAVEL_CODE_QUALITY,
-    ]);
-
-    $rectorConfig->paths([
+    ])
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withImportNames()
+    ->withSkip([
         AddLiteralSeparatorToNumberRector::class,
-        ArrayShapeFromConstantArrayReturnRector::class,
-        ArraySpreadInsteadOfArrayMergeRector::class,
         ClosureToArrowFunctionRector::class,
         CountArrayToEmptyArrayComparisonRector::class,
-        ExplicitMethodCallOverMagicGetSetRector::class,
         IssetOnPropertyObjectToPropertyExistsRector::class,
         JsonThrowOnErrorRector::class,
-    ]);
-
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses();
-};
+    ])
+    ->withRootFiles();
